8f8495906770edbd9ffdcf90a60960a2cd311281
Tictac0723 <taylorclark0723@gmail.com>
2017-05-06 21:59:54 -0500

    got the letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <title>Hangman</title>
-  </head>
-  <body>
 
 
+var challenges=["Tina", "Louise", "Gene", "Linda", "Bob"];
+var challenge= null;
+var word = null;
 
-  </body>
-  </html>
+window.onload = function() {
+    challenge=getRandomChallenge();
+    word = document.getElementById("word");
+};
+
+function getRandomChallenge() {
+	var randomInt=Math.floor((Math.random()*challenges.length) +1);
+	return challenges[randomInt-1];
+}
+
+document.onkeyup = function(e) {
+            if (challenge.includes(e.key)) {
+            	word.textContent=e.key;
+            }
+
+            console.log(word)
+        };
